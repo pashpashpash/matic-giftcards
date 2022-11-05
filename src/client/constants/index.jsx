0c37da691e6ef50b7fa6066e1543ffa5d3ff065b
NightTrek <daniels@dual4t.com>
2022-11-05 13:12:52 -0700

    added network constants file

---
+++ src/client/constants/index.jsx
@@ -0,0 +1,28 @@
+// Shared Constants
+
+const Constants = {
+    contracts: {
+        // ERC20: require('../../../contracts/ERC20.json'),
+        REDEEMABLE_MATIC
+    },
+
+    networks: {
+        1: 'Ethereum',
+        2: 'Morden',
+        3: 'Ropsten',
+        4: 'Rinkeby',
+        42: 'Kovan',
+        137: 'Polygon',
+    },
+
+    units: {
+        weiInEth: 1000000000000000000,
+        seedInEth: 10000,
+    },
+
+    numbers: {
+        UINT32_MAX: 4294967295,
+    },
+};
+
+export default Constants;
