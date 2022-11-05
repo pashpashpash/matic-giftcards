0a11c6a504a71a2448f3c4c923063ac33e41898a
NightTrek <daniels@dual4t.com>
2022-11-05 13:32:36 -0700

    Updated the account nav with a click to login handler for injected connector.

--- src/client/components/AccountNav/index.jsx
+++ src/client/components/AccountNav/index.jsx
@@ -1,35 +1,44 @@
 // @flow
 import * as React from 'react';
 import { useWeb3React } from '@web3-react/core';
-import { useEagerConnect } from './connectors';
+import { useEagerConnect, injected } from './connectors';
 import { useDispatch } from 'react-redux';
 
 import Constants from '../../constants';
 
 import s from './index.less';
 
-
 const prettyEthAccount = (account: string, chunkSize: ?number): string => {
     chunkSize = chunkSize || 4;
     const len = account.length;
     return (
-        account.slice(0, chunkSize + 2) +
-        '...' +
-        account.slice(len - chunkSize)
+        account.slice(0, chunkSize + 2) + '...' + account.slice(len - chunkSize)
     );
 };
 
 const AccountNav = (): React.Node => {
     const web3react = useWeb3React();
     const tried = useEagerConnect();
-    const { account, chainId } = web3react;
+    const { account, chainId, active, activate, library } = web3react;
+    let activatingConnector = null;
 
-
-
-    const handleClick = (e: SyntheticEvent<>): boolean => {
-        // todo handle login
-        return true;
-    };
+    const handleClick = React.useCallback(() => {
+        if (!activate) {
+            console.log('[AccountNav] Error Activate undefined');
+        }
+        if (active) return;
+        activate(injected, (error: Error) => {
+            console.log('[AccountNav] Error inside activate function', {
+                error,
+            });
+        })
+            .then((res): any => {
+                activatingConnector = null;
+            })
+            .catch((err: Error): any => {
+                console.log('[AccountNav] Error activating connector', err);
+            });
+    }, [injected, activate, active, account, library]);
 
     let prettyAccount = 'Connect Wallet';
 
