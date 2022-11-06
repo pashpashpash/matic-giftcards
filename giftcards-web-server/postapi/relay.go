b6fb569051ae2932d93bd63239f8364a8fc3316f
NightTrek <daniels@dual4t.com>
2022-11-05 23:29:37 -0700

    added the first version of the relayer without validation

--- giftcards-web-server/postapi/relay.go
+++ giftcards-web-server/postapi/relay.go
@@ -27,3 +27,53 @@ type OpenZeppelinAutoTasksAPI struct {
 	Result        map[string]interface{} `json:"result,omitempty"`
 	RequestID     string                 `json:"requestId,omitempty"`
 }
+
+
+// RelayRedeemHandler Calls OpenZeppelin relay with provided metatx
+func RelayRedeemHandler(w http.ResponseWriter, r *http.Request) {
+	form := new(form.RelayRedeemForm)
+
+
+
+	// Create Openzeppelin request
+	openZeppelinRequestArguments, err := json.Marshal(form)
+	if err != nil {
+		log.Println("[RelayRedeem] Error marshalling OpenZeppelin request", err)
+		http.Error(w, fmt.Sprintf("[RelayRedeem] Error marshalling OpenZeppelin request"), http.StatusNotFound)
+		return
+	}
+	req, err := http.NewRequest(http.MethodPost, OPENZEPPELIN_WEBHOOK_URI, bytes.NewBuffer(openZeppelinRequestArguments))
+	if err != nil {
+		log.Println("[RelayRedeem] Error forming OpenZeppelin request", err)
+		http.Error(w, fmt.Sprintf("[RelayRedeem] Error forming OpenZeppelin request"), http.StatusNotFound)
+		return
+	}
+	req.Header.Add("X-Api-Key", OPENZEPPELIN_API_KEY)
+
+	// Call Openzeppelin request
+	HTTP_CLIENT = &http.Client{Timeout: 26 * time.Second}
+	openZeppelinResponse, err := HTTP_CLIENT.Do(req)
+	if err != nil {
+		log.Println("[RelayRedeem] Error Fetching OpenZeppelin Data", err)
+		http.Error(w, fmt.Sprintf("[RelayRedeem] Error Fetching OpenZeppelin Data"), http.StatusNotFound)
+		return
+	}
+	openZeppelinDataBytes, err := ioutil.ReadAll(openZeppelinResponse.Body)
+	if err != nil {
+		log.Println("[RelayRedeem] Error Reading OpenZeppelin Data", err)
+		http.Error(w, fmt.Sprintf("[RelayRedeem] Error Reading OpenZeppelin Data"), http.StatusNotFound)
+		return
+	}
+	log.Println("[RelayRedeem] Fetched OpenZeppelin Response Bytes:", string(openZeppelinDataBytes))
+
+	openZeppelinData := OpenZeppelinAutoTasksAPI{}
+	json.Unmarshal(openZeppelinDataBytes, &openZeppelinData)
+	log.Println("[RelayRedeem] Open Zepplin response ", openZeppelinData)
+	if openZeppelinData.Status != "success" {
+		log.Println("[RelayRedeem] Openzeppelin error", openZeppelinData.EncodedLogs, err)
+		http.Error(w, fmt.Sprintf("[RelayRedeem] Relay Error. OpenZeppelin response status has error"), http.StatusNotFound)
+		return
+	}
+
+	log.Println("[RelayRedeem] Completed")
+}
